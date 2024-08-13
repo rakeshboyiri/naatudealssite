7b90f64e45cb8a1c403bae5eb9c2205e7417944e
naatudealsofficial <naatudealsofficial@gmail.com>
2024-08-13 16:26:07 +0530

    logo-changed

--- frontend/src/components/UserPage.js
+++ frontend/src/components/UserPage.js
@@ -48,8 +48,9 @@ const UserPage = () => {
           <img src={logo} alt='logo' className='logo-image'/> <br></br>
           <div className='header-text'>
           <h1>Naatu Deals</h1>
-          <h3>Join Our Telegram Channel & Grab the Deals</h3>
-          <a href="https://t.me/NaatuDeals" target="_blank" rel="noopener noreferrer" className='telegram-link'>Join Our Channel</a>
+          <h3>💥Join Our Telegram Channel & Grab the Deals 💥</h3>
+         <h4>👇🏻👇🏻👇🏻</h4>
+          <button className='telegram-link' onClick={() => window.open('https://t.me/NaatuDeals', '_blank')}><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/1200px-Telegram_logo.svg.png" alt="Telegram" width="20" height="20" /> Join Our Channel</button>
           </div>
          
         </div>
