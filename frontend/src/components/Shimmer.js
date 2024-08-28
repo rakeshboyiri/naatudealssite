36ba090bda0e5e5b4ced550879766c9d2f37a6a8
rakeshboyiri <boyirirakesh.com>
2024-08-29 00:11:20 +0530

    ui-changed

--- frontend/src/components/Shimmer.js
+++ frontend/src/components/Shimmer.js
@@ -2,35 +2,33 @@ const Shimmer = () => {
     return (
         <div className='shimmer'>
             
-            <div className='shimmer-item'>Card 1</div>
-            <div className='shimmer-item'>Card 2</div>
-            <div className='shimmer-item'>Card 3</div>
-            <div className='shimmer-item'>Card 4</div>
-            <div className='shimmer-item'>Card 1</div>
-            <div className='shimmer-item'>Card 2</div>
-            <div className='shimmer-item'>Card 3</div>
-            <div className='shimmer-item'>Card 4</div>
-            <div className='shimmer-item'>Card 4</div> 
-
-            <div className='shimmer-item'>Card 1</div>
-            <div className='shimmer-item'>Card 2</div>
-            <div className='shimmer-item'>Card 3</div>
-            <div className='shimmer-item'>Card 4</div>
-            <div className='shimmer-item'>Card 1</div>
-            <div className='shimmer-item'>Card 2</div>
-            <div className='shimmer-item'>Card 3</div>
-            <div className='shimmer-item'>Card 4</div>
-            <div className='shimmer-item'>Card 4</div> 
-
-            <div className='shimmer-item'>Card 1</div>
-            <div className='shimmer-item'>Card 2</div>
-            <div className='shimmer-item'>Card 3</div>
-            <div className='shimmer-item'>Card 4</div>
-            <div className='shimmer-item'>Card 1</div>
-            <div className='shimmer-item'>Card 2</div>
-            <div className='shimmer-item'>Card 3</div>
-            <div className='shimmer-item'>Card 4</div>
-            <div className='shimmer-item'>Card 4</div> 
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div>
+            <div className='shimmer-item'></div> 
         </div>
     );
 }
